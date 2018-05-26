dbc2c488274ef7d9a34db719d9a74400ff7792df
inkoziev <mentalcomputing@gmail.com>
2018-05-26 15:08:46 +0400

    minor changes in wordchar2vector model dataset preparation: contents of evaluate_relevancy.txt is parsed and added to list of words

--- PyModels/prepare_wordchar_dataset.py
+++ PyModels/prepare_wordchar_dataset.py
@@ -10,23 +10,26 @@ from __future__ import print_function
 
 import codecs
 import itertools
-
+import os
+from sys import platform
 import pandas as pd
 
 from utils.tokenizer import Tokenizer
 
-#data_folder = '../data'
 result_path = '../tmp/known_words.txt' # путь к файлу, где будет сохранен список слов
 
 n_misspelling_per_word = 0 # кол-во добавляемых вариантов с опечатками на одно исходное слово
 
 
 # Из этого текстового файла возьмем слова, на которых будем тренировать модель встраивания.
-#corpus_path = r'f:\Corpus\word2vector\ru\SENTx.corpus.w2v.txt'
-corpus_path = '/home/eek/Corpus/word2vector/ru/SENTx.corpus.w2v.txt'
-#paraphrases_path = '../data/paraphrases.csv'
+if platform == "win32":
+    corpus_path = r'f:\Corpus\word2vector\ru\SENTx.corpus.w2v.txt'
+else:
+    corpus_path = os.path.expanduser('~/Corpus/word2vector/ru/SENTx.corpus.w2v.txt')
+
 paraphrases_path = '../data/premise_question_relevancy.csv'
 pqa_path = '../data/premise_question_answer.csv'
+eval_path = '../data/evaluate_relevancy.txt'
 
 # ---------------------------------------------------------------
 
@@ -58,6 +61,13 @@ for phrase in itertools.chain( df['premise'].values, df['question'].values, df['
     words = tokenizer.tokenize(phrase)
     known_words.update(words)
 
+# Добавим слова, которые употребляются в датасете для оценки
+print('Parsing {}'.format(eval_path))
+with codecs.open(eval_path, 'r', 'utf-8') as rdr:
+    for line in rdr:
+        phrase = line.replace(u'T:', u'').replace(u'Q:', u'').strip()
+        words = tokenizer.tokenize(phrase)
+        known_words.update(words)
 
 print('There are {} known words'.format(len(known_words)))
 
