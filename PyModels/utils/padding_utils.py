497d2af9288fcfcf18a0316e72b0c0f4996cd73b
inkoziev <mentalcomputing@gmail.com>
2018-05-26 15:07:08 +0400

    some utility code for text tokenization etc

---
+++ PyModels/utils/padding_utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+
+import itertools
+
+PAD_WORD = u''
+
+
+def pad_wordseq(words, n):
+    if len(words) >= 0:
+        return words
+    else:
+        return list(itertools.chain(itertools.repeat(PAD_WORD, n-len(words)), words,))
