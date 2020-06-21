1e067ed3a4d1a5658966dd5ed672c893cd36a56b
Ilya Koziev <inkoziev@gmail.com>
2020-06-21 13:29:16 +0300

    добавлено подготовка опорных сэмплов для knn-1 модели интерпретатора

--- ruchatbot/preparation/prepare_interpreter_templates.py
+++ ruchatbot/preparation/prepare_interpreter_templates.py
@@ -2,22 +2,25 @@
 """
 Подготовка датасета для тренировки модели интерпретации ответов в чатботе. Эта модель
 восстанавливает полный (подразумеваемый, развернутый) текст ответа по фактическому
-краткому ответу собеседника, используя контекст в виде заданного вопроса.
+краткому ответу собеседника, используя контекст в виде заданного вопроса. Обрабатывается анафора,
+гэпинг, эллипсис.
 
 28-03-2020 Добавлен отчет по OOV словам в датасете
 30-05-2020 Добавлена загрузка сэмплов из question interpretation датасета в общую кучу
+20-06-2020 Добавлена генерация шаблонов интерпретатора (knn1-модель)
 """
 
 import io
 import collections
 import os
 import re
-
-import sklearn.model_selection
+import pickle
+import itertools
 
 import rutokenizer
 import rupostagger
 import ruword2tags
+import rulemma
 
 
 def is_int(s):
@@ -185,6 +188,156 @@ def wx(s):
     return ' '.join(tokenizer.tokenize(s)).lower()
 
 
+def is_important_token2(pos, lemma):
+    if pos in ('NOUN', 'VERB', 'ADJ', 'ADV', 'PRON', 'NUM'):
+        return True
+
+    if lemma in ('да', 'нет', 'не', 'ни', 'ага'):
+        return True
+
+    return False
+
+
+salient_tags = 'Case Gender Number Tense Person Mood'.split()
+
+
+class ContextTemplateItem:
+    def __init__(self, form, lemma, tags, location):
+        self.form = form
+        self.lemma = lemma
+        self.tags = tags
+        self.location = location
+
+    def __repr__(self):
+        s = self.form
+        if self.tags:
+            s += '(' + '|'.join(self.tags) + ')'
+            s += '@{};{}'.format(self.location[0], self.location[1])
+        return s
+
+
+class ContextTemplateLine:
+    def __init__(self, source_str, items):
+        self.source_str = source_str
+        self.items = items
+
+    def __repr__(self):
+        return self.source_str
+
+    def generate_packed_template(self):
+        res = []
+        for item in self.items:
+            if item.tags:
+                res.append((None, item.tags, item.location))
+            else:
+                res.append((item.form, None, None))
+        return tuple(res)
+
+
+def create_context_template(context_line_index, source_str, expanded_tokens):
+    items = []
+    expanded_lemmas = [t[2] for t in expanded_tokens]
+
+    tokens = lemmatizer.lemmatize(tagger.tag(tokenizer.tokenize(source_str)))
+
+    for token_index, token in enumerate(tokens):
+        lemma = token[2]
+        tags = token[1].split('|')
+        pos = tags[0]
+        if is_important_token2(pos, lemma) or len(tokens) == 1:
+            item = None
+            if pos in ('NOUN', 'ADJ', 'VERB', 'ADV', 'NUM'):
+                if lemma in expanded_lemmas:
+                    # Это слово уходит в результат
+                    # Оставим только проверку важных тэгов
+                    tags = tuple([pos] + list(filter(lambda t: t.split('=')[0] in salient_tags, tags)))
+                    item = ContextTemplateItem(token[0], lemma, tags, (context_line_index, token_index))
+
+            if item is None:
+                item = ContextTemplateItem(token[0], lemma, None, None)
+
+            items.append(item)
+
+    if len(items) == 0:
+        print('ERROR@225: empty template generated for "{}"'.format(' '.join(t[0] for t in tokens)))
+        exit(0)
+
+    return ContextTemplateLine(source_str, items)
+
+
+def create_expanded_template(context_templates, expanded_tokens):
+    items = []
+    for token in expanded_tokens:
+        lemma = token[2]
+        tags = token[1]
+        item2 = None
+        # Ищем, откуда из контекста можно скопировать эту лемму
+        for template in context_templates:
+            for item_index, item in enumerate(template.items):
+                if item.lemma and item.lemma == lemma:
+                    out_tags = [tags.split('|')[0]]
+                    for tag in tags.split('|')[1:]:
+                        tname, tval = tag.split('=')
+                        if tname in salient_tags:
+                            if tname == 'Number':
+                                if tval == 'Sing':
+                                    out_tags.append(('ЧИСЛО', 'ЕД'))
+                                else:
+                                    out_tags.append(('ЧИСЛО', 'МН'))
+                            elif tname == 'Case':
+                                if tval == 'Nom':
+                                    out_tags.append(('ПАДЕЖ', 'ИМ'))
+                                elif tval == 'Gen':
+                                    out_tags.append(('ПАДЕЖ', 'РОД'))
+                                elif tval == 'Ins':
+                                    out_tags.append(('ПАДЕЖ', 'ТВОР'))
+                                elif tval == 'Acc':
+                                    out_tags.append(('ПАДЕЖ', 'ВИН'))
+                                elif tval == 'Dat':
+                                    out_tags.append(('ПАДЕЖ', 'ДАТ'))
+                                elif tval == 'Loc':
+                                    out_tags.append(('ПАДЕЖ', 'ПРЕДЛ'))
+                            elif tname == 'Gender':
+                                if tval == 'Fem':
+                                    out_tags.append(('РОД', 'ЖЕН'))
+                                elif tval == 'Masc':
+                                    out_tags.append(('РОД', 'МУЖ'))
+                                else:
+                                    out_tags.append(('РОД', 'СР'))
+                            elif tname == 'Tense':
+                                if tval == 'Past':
+                                    out_tags.append(('ВРЕМЯ', 'ПРОШЕДШЕЕ'))
+                                elif tval == 'Pres':
+                                    out_tags.append(('ВРЕМЯ', 'НАСТОЯЩЕЕ'))
+                                else:
+                                    out_tags.append(('ВРЕМЯ', 'БУДУЩЕЕ'))
+                            elif tname == 'Person':
+                                if tval == '1':
+                                    out_tags.append(('ЛИЦО', '1'))
+                                elif tval == '2':
+                                    out_tags.append(('ЛИЦО', '2'))
+                                else:
+                                    out_tags.append(('ЛИЦО', '3'))
+                            elif tname == 'Mood':
+                                if tval == 'Imp':
+                                    out_tags.append(('НАКЛОНЕНИЕ', 'ПОБУД'))
+                                elif tval == 'Ind':
+                                    out_tags.append(('НАКЛОНЕНИЕ', 'ИЗЪЯВ'))
+
+                    item2 = (None, item.location, tuple(out_tags))
+                    break
+
+            if item2:
+                break
+
+        if item2 is None:
+            item2 = (token[0], None, None)
+
+        items.append(item2)
+
+    return tuple(items)
+
+
 if __name__ == '__main__':
     data_dir = '../../data'
     tmp_dir = '../../tmp'
@@ -290,7 +443,7 @@ if __name__ == '__main__':
     # Сформируем отчет о самых редких интерпретациях, чтобы можно было аугментировать их руками и нарастить частоту.
     with io.open(rare_path, 'w', encoding='utf-8') as wrt:
         rare_outputs = set(output for output, freq in output2freq.items() if freq == 1)
-        for sample in samples2:
+        for sample in sorted(samples2, key=lambda s: (len(s.short_phrase), len(s.left))):
             if sample.expanded_phrase in rare_outputs:
                 for context_phrase in sample.left.split('|'):
                     wrt.write('{}\n'.format(context_phrase.strip()))
@@ -338,8 +491,8 @@ if __name__ == '__main__':
     #tags = list(tagger.tag(words))
     # КОНЕЦ ОТЛАДКИ
 
-    #lemmatizer = rulemma.Lemmatizer()
-    #lemmatizer.load()
+    lemmatizer = rulemma.Lemmatizer()
+    lemmatizer.load()
 
     all_templates = set()
     template2freq = collections.Counter()
@@ -400,3 +553,40 @@ if __name__ == '__main__':
                             wrt.write('\n\n')
                             oov_tokens.add(uword)
                             break
+
+    # Делаем морфологические шаблоны для модели интерпретации knn-1
+    print('Building knn1 templates from {} samples...'.format(len(samples2)))
+    templates2 = collections.Counter()
+    packed2samples = collections.defaultdict(list)
+    for sample in samples2:
+        # НАЧАЛО ОТЛАДКИ
+        if 'зовут' not in sample.left or sample.short_phrase.lower() != 'илья':
+            continue
+        # КОНЕЦ ОТЛАДКИ
+
+        context = [s.strip() for s in sample.left.split('|')] + [sample.short_phrase]
+        expanded_tokens = lemmatizer.lemmatize(tagger.tag(tokenizer.tokenize(sample.expanded_phrase)))
+        context_templates = [create_context_template(iline, line_str, expanded_tokens) for iline, line_str in enumerate(context)]
+        expanded_template = create_expanded_template(context_templates, expanded_tokens)
+
+        # выкидываем из контекста леммы, так они были нужны только для
+        context_templates = tuple(t.generate_packed_template() for t in context_templates)
+        k = (context_templates, expanded_template)
+        templates2[k] += 1
+        packed2samples[k].append(sample)
+
+    print('{} template samples created'.format(len(templates2)))
+    # отсортируем шаблоны в порядке убывания частоты
+    templates2 = [t for t, freq in templates2.most_common()]
+    with open(os.path.join(tmp_dir, 'interpreter_templates2.bin'), 'wb') as f:
+        pickle.dump(templates2, f)
+
+    with io.open(os.path.join(tmp_dir, 'interpreter_templates2.txt'), 'w', encoding='utf-8') as wrt:
+        for key, samples in packed2samples.items():
+            wrt.write('template:\n')
+            for i, context in enumerate(key[0]):
+                wrt.write('#{}: {}\n'.format(i, context))
+            wrt.write('output: {}\n\n'.format(key[1]))
+
+            for sample in samples:
+                wrt.write('{}\n{}\n{}\n\n'.format(sample.left.replace(' | ', '\n'), sample.short_phrase, sample.expanded_phrase))
