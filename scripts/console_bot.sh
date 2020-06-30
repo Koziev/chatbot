67b4f97a45600eaf5e35d78dfbd38aa054d78cfd
Ilya Koziev <inkoziev@gmail.com>
2020-06-30 09:10:17 +0300

    для консольного бота отключено детальное логирование, чтобы не засорять консоль

--- scripts/console_bot.sh
+++ scripts/console_bot.sh
@@ -1,3 +1,4 @@
 # profile_1.json это файл с настройками бота; там задаются пути к файлам с фактами и т.д.
-PYTHONPATH=.. python3 ../ruchatbot/frontend/console_chatbot.py --profile ../data/profile_1.json --debugging --data_folder ../data --models_folder ../tmp --w2v_folder ../tmp --tmp_folder ../tmp
+# флаг --debugging включит детальное логирование
+PYTHONPATH=.. python3 ../ruchatbot/frontend/console_chatbot.py --profile ../data/profile_1.json --data_folder ../data --models_folder ../tmp --w2v_folder ../tmp --tmp_folder ../tmp
 
