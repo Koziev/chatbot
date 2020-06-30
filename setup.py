9e6bc696500d99e870f2b2b39fd179e2575a7da6
Ilya Koziev <inkoziev@gmail.com>
2020-06-30 15:24:41 +0300

    новый номер версии 0.0.2.14

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="ruchatbot",
-    version="0.0.2.13",
+    version="0.0.2.14",
     author="Ilya Koziev",
     author_email="inkoziev@gmail.com",
     description="Chatbot, NLU, dialogue management",
