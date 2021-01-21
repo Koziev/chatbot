ea31000e776a1f9f3873eda4d22fc52b4bf4271a
koziev ilya <inkoziev@gmail.com>
2021-01-21 15:36:56 +0300

    новый тип сценариев с графами переходов

--- ruchatbot/bot/scenario.py
+++ ruchatbot/bot/scenario.py
@@ -1,13 +1,16 @@
 # coding: utf-8
 """
 Сценарии - автономные, долгоживущие фрагменты диалога, выполняющие управление дискурсом.
-07.12.2020 - добавлена секции "pool" со списком вопросов
+
+20.01.2021 Реализация графовых сценариев с вероятностными условными переходами
 """
 
 import random
 import logging
 
-from ruchatbot.bot.actors import ActorBase
+from ruchatbot.bot.actors import ActorBase, ActorSay
+from ruchatbot.bot.base_rule_condition import BaseRuleCondition
+
 from ruchatbot.bot.smalltalk_rules import SmalltalkRules
 from ruchatbot.bot.scripting_rule import ScriptingRule
 from ruchatbot.bot.running_scenario import RunningScenario
@@ -24,6 +27,59 @@ class ScenarioTerminationPolicy:
             self.expiration = int(yaml_node['expiration'])
         if 'can_answer_question' in yaml_node:
             self.can_answer_question = replace_constant(yaml_node['can_answer_question'], constants, text_utils)
+
+
+class ScenarioTransition:
+    def __init__(self):
+        self.condition = None
+        #self.proba = None
+        self.next_step = None
+
+    @staticmethod
+    def load_yaml(yaml_node, constants, text_utils):
+        t = ScenarioTransition()
+        t.next_step = yaml_node['goto']
+        if 'keyword' in yaml_node or 'text' in yaml_node:
+            t.condition = BaseRuleCondition.from_yaml(yaml_node, constants, text_utils)
+        else:
+            raise RuntimeError()
+        return t
+
+
+class ScenarioStep:
+    def __init__(self):
+        self.name = None
+        self.next_step_names = None
+        self.action = None
+        self.transitions = []
+
+    @staticmethod
+    def load_yaml(yaml_node, constants, text_utils):
+        step = ScenarioStep()
+        step.name = yaml_node['name']
+        step.say = ActorSay.from_yaml(yaml_node['say'], constants, text_utils)
+
+        if 'default_next' in yaml_node:
+            if isinstance(yaml_node['default_next'], str):
+                step.next_step_names = [yaml_node['default_next']]
+            else:
+                step.next_step_names = list(yaml_node['default_next'])
+
+        if 'transitions' in yaml_node:
+            for tr_yaml in yaml_node['transitions']:
+                step.transitions.append(ScenarioTransition.load_yaml(tr_yaml['transition'], constants, text_utils))
+
+        return step
+
+    @staticmethod
+    def from_say_actor(step_index, step_node, constants, text_utils):
+        step = ScenarioStep()
+        step.name = str(step_index)
+        step.say = ActorBase.from_yaml(step_node, constants, text_utils)
+        return step
+
+    def get_name(self):
+        return self.name
 
 
 class Scenario(object):
@@ -63,7 +119,9 @@ class Scenario(object):
             scenario.priority = 10  # дефолтный уровень приоритета
 
         if 'steps_policy' in yaml_node:
-            scenario.steps_policy = yaml_node['steps_policy']  # TODO - сделать проверку значения
+            scenario.steps_policy = yaml_node['steps_policy']
+            if scenario.steps_policy not in 'sequential random graf'.split():
+                raise RuntimeError('Scenario "{}" loading error: unknown step_policy "{}"'.format(scenario.name, scenario.steps_policy))
         else:
             scenario.steps_policy = 'sequential'
 
@@ -73,9 +131,14 @@ class Scenario(object):
             scenario.chitchat_questions_per_step_rate = 0
 
         if 'steps' in yaml_node:
-            for step_node in yaml_node['steps']:
-                step = ActorBase.from_yaml(step_node, constants, text_utils)
-                scenario.steps.append(step)
+            if scenario.steps_policy == 'graf':
+                for step_node in yaml_node['steps']:
+                    step = ScenarioStep.load_yaml(step_node['step'], constants, text_utils)
+                    scenario.steps.append(step)
+            else:
+                for step_node in yaml_node['steps']:
+                    step = ScenarioStep.from_say_actor(len(scenario.steps), step_node, constants, text_utils)
+                    scenario.steps.append(step)
 
         if 'on_start' in yaml_node:
             scenario.on_start = ActorBase.from_yaml(yaml_node['on_start'], constants, text_utils)
@@ -113,6 +176,9 @@ class Scenario(object):
     def is_sequential_steps(self):
         return self.steps_policy == 'sequential'
 
+    def is_graf(self):
+        return self.steps_policy == 'graf'
+
     def get_chitchat_questions_per_step_rate(self):
         return self.chitchat_questions_per_step_rate
 
@@ -130,7 +196,61 @@ class Scenario(object):
         # пометки о пройденных шагах и прочая информация, которая уже не будет нужна после завершения сценария.
         assert(isinstance(running_scenario, RunningScenario))
 
-        if self.is_sequential_steps():
+        if self.is_graf():
+            next_step_name = None
+            transition_found = False
+
+            # Смотрим не текущий узел в графе
+            if running_scenario.current_step_index == -1:
+                # Это вход в граф. Потом надо сделать более гибкий механизм поиска стартового узла,
+                # а сейчас просто возьмем первый в списке и выполним его (он скажет что-то.)
+                start_index = 0  # !!! TODO !!!
+                next_step_name = self.steps[start_index].get_name()
+                transition_found = True
+                logging.debug('Entering scenario "%s" to node "%s"', self.get_name(), next_step_name)
+            else:
+                current_step = self.steps[running_scenario.current_step_index]
+                logging.debug('Continuing scenario "%s" from node "%s"', self.get_name(), current_step.get_name())
+
+                # У него есть условные переходы?
+                if current_step.transitions:
+                    # Проверяем, не должны ли мы перейти по одному из этих условных переходов.
+                    for tr in current_step.transitions:
+                        if tr.condition.check_condition(bot, session, interlocutor, interpreted_phrase, bot.get_engine()):
+                            next_step_name = tr.next_step
+                            break
+
+                if next_step_name is None and current_step.next_step_names:
+                    # Дефолтный переход на один из указанных узлов
+                    next_step_name = random.choice(current_step.next_step_names)
+
+            # Ищем этот узел в графе ...
+            if next_step_name and next_step_name != '_EXIT_':
+                found = False
+                for inode, node in enumerate(self.steps):
+                    if node.get_name() == next_step_name:
+                        # Нашли!
+                        found = True
+                        running_scenario.current_step_index = inode
+                        logging.debug('Transition to node "%s" in scenario "%s"', next_step_name, self.get_name())
+                        transition_found = True
+                        step_ok = node.say.do_action(bot, session, interlocutor, interpreted_phrase, None, text_utils)
+                        break
+
+                if not found:
+                    logging.error('Node "%s" not found in scenario "%s"', next_step_name, self.get_name())
+
+            if not transition_found:
+                # По умолчанию - выходим из сценария...
+                logging.debug('Dead-end in scenario "%s"', self.get_name())
+
+                # Уберем инстанс сценария из списка активных
+                self.termination_check_count = 0
+                if running_scenario.scenario.on_finish:
+                    running_scenario.scenario.on_finish.do_action(bot, session, interlocutor, interpreted_phrase, None, text_utils=text_utils)
+                bot.get_engine().exit_scenario(bot, session, interlocutor, interpreted_phrase)
+
+        elif self.is_sequential_steps():
             # Шаги сценария выполняются в заданном порядке
             while True:
                 new_step_index = running_scenario.current_step_index + 1
@@ -150,7 +270,7 @@ class Scenario(object):
                     logging.debug('Executing step #%d in scenario "%s"', new_step_index, self.get_name())
                     step = running_scenario.scenario.steps[new_step_index]
                     running_scenario.passed_steps.add(new_step_index)
-                    step_ok = step.do_action(bot, session, interlocutor, interpreted_phrase, None, text_utils)
+                    step_ok = step.say.do_action(bot, session, interlocutor, interpreted_phrase, None, text_utils)
                     if step_ok:
                         break
 
@@ -162,7 +282,7 @@ class Scenario(object):
             logging.debug('Executing step #%d in scenario "%s"', new_step_index, self.get_name())
             running_scenario.passed_steps.add(new_step_index)
             step = running_scenario.scenario.steps[new_step_index]
-            step.do_action(bot, session, interlocutor, interpreted_phrase, None, text_utils=text_utils)
+            step.say.do_action(bot, session, interlocutor, interpreted_phrase, None, text_utils=text_utils)
 
             do_terminate = False
             if len(running_scenario.passed_steps) == nsteps:
