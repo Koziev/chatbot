5855353e9efb194c339c8192a142a709fa151dd0
koziev ilya <inkoziev@gmail.com>
2021-12-12 14:00:51 +0300

    refs #45 добавлены скрипты для запуска rest api и телеграм-бота

---
+++ scripts/flask_bot.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=.. python3 ../ruchatbot/frontend/flask_service_bot.py --profile ../data/profile_1.json --data_folder ../data --models_folder ../tmp --w2v_folder ../tmp --tmp_folder ../tmp --ip 0.0.0.0
